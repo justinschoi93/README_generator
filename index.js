d3987d22912fb71b406d3716076f0afb587beb59
justinschoi93 <justinschoi93@gmail.com>
2023-07-24 17:13:18 -0700

    .then updated. generateREADMEText started. README restored.

--- index.js
+++ index.js
@@ -4,39 +4,39 @@ const fs = require('fs');
 inquirer
     .prompt([
         {
-            question: 'What would you like the title of your project to be?',
+            message: 'What would you like the title of your project to be?',
             name: 'title',
         },
         {
-            question:'Describe your project...',
+            message:'Describe your project...',
             name: 'description'
         },
         {
-            question: 'How does one install your application?',
+            message: 'How does one install your application?',
             name: 'installation-steps'
         },
         {
-            question: 'How does one use your application?',
+            message: 'How does one use your application?',
             name: 'directions'
         },
         {
-            question: 'Who contributed to the development of this application?',
+            message: 'Who contributed to the development of this application?',
             name: 'contributors'
         },
         {
-            question: 'How does one test the functionality of your application?',
+            message: 'How does one test the functionality of your application?',
             name: 'tests'
         },
         {
-            question: 'What is your gitHub username?',
+            message: 'What is your gitHub username?',
             name: 'gitHub'
         },
         {
-            question: 'What is your email address?',
+            message: 'What is your email address?',
             name: 'email'
         },
         {
-            question: 'Choose a license for your application',
+            message: 'Choose a license for your application',
             name: 'license',
             type: 'list',
             choices: [
@@ -69,30 +69,21 @@ inquirer
 
         }
     ]).then(function(response){
-        console.log(response)
+        
+        var text = generateREADMEText(response);
+       
+       
+
+        fs.writeFile('README.md', JSON.stringify(text), (error) => {
+            error ? console.log(error) : console.log('success!')
+        })
     })
-    // 
 
-//README gets generated after a series of prompts and questions.
-
-// Ask user if they would like to create a README
-    // - if yes...
-// Ask for title,
-//     - add title to title section
-// - autogenerate following sections
-//     - description
-//     - installation instructions
-//     - usage information
-    // - Contributions list
-    // - Questions
-    // - Tests?
-// - provide user with option to choose a license from a list
-//     - badge gets added near top of README
-//     - notice gets added to License section
-// - Ask for github username
-//     - add to Questions section, with link to github profile
-// - Ask for email address
-//     - add to Questions section
-//     - also include instructions on how to reach me with additional Questions
-// - Turn table of Contents into links
-
+function generateREADMEText(data) {
+    var README = '
+    #
+    
+    
+    ';
+    return README;
+}
